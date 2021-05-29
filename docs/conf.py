d0fc129922a9000c5fc4bed78a2766001adeb1a1
Tyler McMaster <tyler@tlm13.com>
2021-05-29 18:15:31 -0400

    looking at documenatation
    looking at documenatation

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,15 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import os, sys
+import alabaster
+
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 #
-import os, sys
-import alabaster
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../src'))
 
 import deftlariat
 
