e1938162b940894b35198a72fb8d0c11dd2939c2
Tyler McMaster <tyler@tlm13.com>
2021-05-29 22:36:40 -0400

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mcmasty/PyDeftLariats',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
