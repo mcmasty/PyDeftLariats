5322927cb8779d36a4ade4ef1299f55d35a58c49
mcmasty@outcastsystems.com <mcmasty@outcastsystems.com>
2025-05-23 18:08:35 -0400

    update docs and exmaples

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mcmasty/PyDeftLariats',
-    version='1.2.10',
+    version='1.2.11',
     zip_safe=False,
 )
